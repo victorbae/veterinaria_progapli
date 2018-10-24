4ed8ba5f3e5407df157af563d198de5525ef29e4
Victor Baesso <jotatyr@gmail.com>
2018-10-24 21:45:12 -0200

    Mais um Teste

---
+++ Veterinaria/src/br/com/unoesc/veterinaria/staticos/auxiliares/EstaticosParaGeral.java
@@ -0,0 +1,9 @@
+package br.com.unoesc.veterinaria.staticos.auxiliares;
+
+import javafx.scene.layout.BorderPane;
+
+public class EstaticosParaGeral {
+
+	public static BorderPane bpPrincipalAux;
+
+}
