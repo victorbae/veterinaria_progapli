604e5114e0cd1dec95c32a0dd5fe3db0829fdfbd
Victor Baesso <jotatyr@gmail.com>
2018-10-24 21:46:57 -0200

    564

--- Veterinaria/src/br/com/unoesc/veterinaria/soParaDarCommit/SoParaDarCommit.java
+++ Veterinaria/src/br/com/unoesc/veterinaria/soParaDarCommit/SoParaDarCommit.java
@@ -2,4 +2,9 @@ package br.com.unoesc.veterinaria.soParaDarCommit;
 
 public class SoParaDarCommit {
 //Comentários ou qualquer bosta para podermos dar commitddddddd
+	/*
+	 * 
+	 * Porque
+	 * 
+	 */
 }
