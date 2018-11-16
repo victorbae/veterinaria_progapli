ff98de5943e2a28d014c88972449c79ab0d333da
vpsg_ <vpsg_@172.18.23.220>
2018-11-16 19:45:54 -0200

    Vitão da casa própria

--- Veterinaria/query/Veterinaria_SQL_08_11_2018.sql
+++ Veterinaria/query/Veterinaria_SQL_08_11_2018.sql
@@ -8,17 +8,8 @@ CREATE TABLE Filial (
   id_Gerente INT NULL,
   Telefone VARCHAR(45) NULL,
   CNPJ VARCHAR(45) NULL,
-  capacidadeEstoque int
+  CapacidadeEstoque DECIMAL(12,2) NOT NULL
   );
-
-
-CREATE TABLE Estoque (
-  idEstoque INT NOT NULL auto_increment primary key,
-  Capacidade VARCHAR(45) NULL,
-  idFilial INT NOT NULL,
-  
-  CONSTRAINT fk_Estoque_Filial1 FOREIGN KEY (idFilial) REFERENCES Filial (idFilial)
-);
 
 CREATE TABLE Produto(
   idProduto INT NOT NULL auto_increment primary key,
@@ -26,9 +17,9 @@ CREATE TABLE Produto(
   Quantidade_Estoque VARCHAR(45) NULL,
   Valor_Entrada_Unt VARCHAR(45) NULL,
   Margem_Lucro VARCHAR(45) NULL,
-  idEstoque INT,
+  idFilial INT NOT NULL,
   
-  CONSTRAINT fk_Produto_Estoque FOREIGN KEY (idEstoque) REFERENCES Estoque (idEstoque)
+  CONSTRAINT fk_Produto_Filial FOREIGN KEY (idFilial) REFERENCES Filial (idFilial)
 );
 
 
@@ -65,18 +56,19 @@ CREATE TABLE Tipo_Produto(
   CONSTRAINT fk_Tipo_Produto_Produto FOREIGN KEY (idProduto) REFERENCES Produto (idProduto)
 );
 
-CREATE TABLE Raca(
-  idRaca INT NOT NULL auto_increment primary key,
-  Nome VARCHAR(45) NULL
-  );
-
 CREATE TABLE Tipo_Animal(
   idTipo_Animal INT NOT NULL auto_increment primary key,
-  Nome VARCHAR(45) NULL,
-  idRaca INT NOT NULL,
-  
-  CONSTRAINT fk_Tipo_Animal_Raca FOREIGN KEY (idRaca) REFERENCES Raca (idRaca)
+  Nome VARCHAR(45) NULL
+
 );
+
+CREATE TABLE Raca(
+  idRaca INT NOT NULL auto_increment primary key,
+  Nome VARCHAR(45) NULL,
+  idTipoAnimal INT NOT NULL,
+  
+  CONSTRAINT fk_Tipo_Animal_Raca FOREIGN KEY (idTipoAnimal) REFERENCES Tipo_Animal (idTipo_Animal)
+  );
 
 CREATE TABLE Animal(
   idAnimal INT NOT NULL auto_increment primary key,
@@ -120,17 +112,6 @@ CREATE TABLE Venda_Produto(
   
   CONSTRAINT fk_Venda_Produto_Produto FOREIGN KEY (idProduto) REFERENCES Produto (idProduto)
     );
-    
-    
-    
-CREATE TABLE funcionario_filial(
-	id int not null primary key auto_increment,
-	idFilial int not null,
-    idFuncionario int not null,
-    
-    foreign key(idFilial) references filial(idFilial),
-    foreign key(idFuncionario) references funcionario(idFuncionario)
-	);
 
 create or replace view lista_dados_vendaProduto as select vp.idVenda as Id_Venda, vp.idProduto as Id_Produto, vp.idVenda_Produto as Id_Venda_Produto,
 	vp.Quantidade as Quantidade, vp.Valor_Unitario as Valor_Unitario, vp.Valor_Total as Valor_Total from Venda_Produto vp;
@@ -141,20 +122,19 @@ v.idFilial as Id_Filial, v.valorTotal as Valor_Total from Venda v;
 CREATE OR REPLACE VIEW lista_dados_cliente AS SELECT cl.idCliente AS Id_Cliente, cl.Nome_Completo AS Nome_Completo, cl.CPF AS CPF, cl.Data_Nascimento AS Data_Nascimento, 
 cl.Endereco AS Endereco, cl.Telefone AS Telefone, cl.idFilial AS Id_Filial FROM Cliente cl;
 
-create or replace view view_lista_funcionarios as select idFuncionario as 'Id_Funcionario', Nome as 'Nome', CPF as 'CPF', Data_Nascimento, id_Cliente as 'Id_Cliente',
-idFilial as 'Id_Filial', email as 'email', senha as 'senha'
-from funcionario;
+create or replace view view_lista_funcionarios as select idFuncionario as Id_Funcionario, Nome as Nome, CPF as CPF, Data_Nascimento, id_Cliente as Id_Cliente,
+idFilial as Id_Filial, email as email, senha as senha from funcionario;
 
-create or replace view view_lista_filiais as select idFilial as 'Id_Filial', Nome, Endereco, id_Gerente as 'Id_Gerente', Telefone, CNPJ, capacidadeEstoque from filial;
+create or replace view view_lista_filiais as select idFilial as Id_Filial, Nome, Endereco, id_Gerente as Id_Gerente, Telefone, CNPJ, CapacidadeEstoque as Capacidade_Estoque from filial;
 
-CREATE OR REPLACE VIEW lista_dados_produto AS SELECT pd.idProduto AS Id_Produto, pd.Nome AS Nome, pd.Quantidade_Estoque AS Qnt_Estoque, 
-pd.Valor_Entrada_Unt AS Valor_Ent_Unt, pd.Margem_Lucro AS Margem_Lucro, pd.idEstoque AS Id_Estoque FROM Produto pd;
+CREATE OR REPLACE VIEW lista_dados_produto AS SELECT pd.idProduto AS Id_Produto, pd.Nome AS Nome, pd.Quantidade_Estoque AS Quantidade_Estoque, 
+pd.Valor_Entrada_Unt AS Valor_Ent_Unt, pd.Margem_Lucro AS Margem_Lucro, pd.idFilial AS idFilial FROM Produto pd;
 
 CREATE OR REPLACE VIEW lista_dados_animal AS SELECT ani.idAnimal AS Id_Animal, ani.Nome AS Nome, ani.Data_Nascimento AS Data_Nascimento, ani.idTipo_Animal AS idTipo_Animal, ani.idCliente AS idCliente,ani.idRaca as idRaca FROM animal ani;
 
-CREATE OR REPLACE VIEW lista_dados_tipo_animal AS SELECT tp.idTipo_Animal AS idTipoAnimal, tp.Nome AS Nome, tp.idRaca AS Id_Raca  FROM tipo_animal tp;
+CREATE OR REPLACE VIEW lista_dados_tipo_animal AS SELECT tp.idTipo_Animal AS idTipoAnimal, tp.Nome AS Nome FROM tipo_animal tp;
 
-CREATE OR REPLACE VIEW lista_dados_raca AS SELECT ra.idRaca AS Id_Raca, ra.nome AS Nome FROM raca ra;
+CREATE OR REPLACE VIEW lista_dados_raca AS SELECT ra.idRaca AS Id_Raca, ra.nome AS Nome, ra.idTipoAnimal AS idTipoAnimal FROM raca ra;
 
 use delimiter $ 
  create trigger trg_apaga_resto_de_venda before delete on venda for each row
@@ -164,19 +144,62 @@ use delimiter $
     
  end $
  use delimiter ;
+
  
- insert into Filial(Nome, Endereco, Telefone, CNPJ, capacidadeEstoque)values("Filial ROOT", "Endereco ROOT", "666666666", "666666666666",666666);
+ insert into Filial(Nome, Endereco, Telefone, CNPJ,CapacidadeEstoque)values("Filial ROOT", "Endereco ROOT", "666666", "666666", 666666);
  
- insert into funcionario(Nome, CPF,Data_Nascimento,idFilial,email,senha)values("Funcionário ROOT", "66666666666", "2018-11-08","1", "root", "root");
+ insert into funcionario(Nome, CPF,Data_Nascimento,idFilial,email,senha)values("Funcionário ROOT", "666666", "2018-11-08","1", "root", "root");
  
- insert into Cliente(Nome_Completo, CPF, Data_Nascimento, Endereco, Telefone, idFilial) values("Cliente ROOT", "666666666", "2018-11-08", "Endereco ROOT", "66666666", 1);
+ insert into Cliente(Nome_Completo, CPF, Data_Nascimento, Endereco, Telefone, idFilial) values("Cliente ROOT", "666666", "2018-11-08", "Endereco ROOT", "666666", 1);
  
  update Filial set id_Gerente = 1 where idFilial = 1;
  
- INSERT INTO funcionario_filial(idFuncionario, idFilial) VALUES (1,1);
+ /*
  
- 
- 
- 
+use delimiter $ 
+create trigger tr_atualiza_qnt_animal_raca_insert after insert on animal for each row
+begin 
+	call atualiza_qnt_animal_raca(new.idRaca);
+end $
 
+create trigger tr_atualiza_qnt_animal_raca_delete after delete on animal for each row
+begin 
+	call atualiza_qnt_animal_raca(old.idRaca);
+end $
+
+create trigger tr_atualiza_qnt_animal_raca_update after update on animal for each row
+begin 
+	if new.raca != old.raca then
+		call atualiza_qnt_animal_raca(new.idRaca);
+    end if;
+end $
+
+create procedure atualiza_qnt_animal_raca(_id int)
+begin 
+	
+    update raca set qntAnimais = quantos_animais_por_raca(_id) where idRaca = _id;
+end
+
+create function quantos_animais_por_raca(_id) returns int
+begin
+	declare _guarda_qnt int;
+    
+	select count(idAnimal) into _guarda_qnt from animal where idRaca = _id;
+    
+    return _guarda_qnt;
+end
+use delimiter ;
+
+ 
+ SELECT * FROM animal a JOIN tipo_animal ta ON a.idTipo_Animal = ta.idTipo_Animal join raca ra ON ta.idRaca = ra.idRaca
+ */
+ 
+CREATE TABLE funcionario_filial(
+	id int not null primary key auto_increment,
+	idFilial int not null,
+    idFuncionario int not null,
+    
+    foreign key(idFilial) references filial(idFilial),
+    foreign key(idFuncionario) references funcionario(idFuncionario)
+	);
  
